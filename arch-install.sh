76893053d69488432cca6e40bbeeb2497a68d8af
code pumpkin <code-pumpkin@outlook.com>
2026-03-26 21:04:03 +0400

    Bug fixes

--- arch-install.sh
+++ arch-install.sh
@@ -1882,7 +1882,7 @@ export XDG_CURRENT_DESKTOP=i3
 # Source system xinitrc.d scripts (D-Bus, systemd user env, etc.)
 if [ -d /etc/X11/xinit/xinitrc.d ]; then
     for f in /etc/X11/xinit/xinitrc.d/?*.sh; do
-        [ -x "$f" ] && . "$f"
+        [ -x "\$f" ] && . "\$f"
     done
 fi
 
